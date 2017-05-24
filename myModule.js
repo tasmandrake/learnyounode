5343e3e78760c036d79cf6cf71360106ae72c572
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-05-24 15:09:44 -0600

    day 2 commit

--- myModule.js
+++ myModule.js
@@ -1,9 +1,15 @@
-module.exports = function (dirName, ext, callback) {
-  const fs = require('fs');
-  fs.readdir(dirName, (err, list) => {
-    if (err) {
-      return callback(err);
-    }
-    return callback(err, list, ext);
-  });
-}
+// 6
+// const path = require('path');
+// const fs = require('fs');
+//
+// module.exports = function (dirName, ext, callback) {
+//   fs.readdir(dirName, function (err, list) {
+//     if (err) {
+//       return callback(err)
+//     }
+//   list = list.filter(element => path.extname(element) === '.' + ext);
+//   callback(null, list)
+//   });
+// }
+
+// 7
--- program.js
+++ program.js
@@ -1,5 +1,7 @@
+// 1
 // console.log("HELLO WORLD");
 
+// 2
 // var sum = 0;
 // for (let i = 0; i < process.argv.length; i++) {
 //   if (Number(process.argv[i])) {
@@ -8,10 +10,12 @@
 // }
 // console.log(sum);
 
+// 3
 // const fs = require('fs');
 // const file = fs.readFileSync(process.argv[2]);
 // console.log(file.toString().split('\n').length - 1);
 
+// 4
 // const fs = require('fs');
 // fs.readFile(process.argv[2], (err, data) => {
 //   if (err) {
@@ -21,6 +25,7 @@
 //   console.log(breaks);
 // });
 
+// 5
 // const fs = require('fs');
 // const path = require('path');
 // const extension = '.' + process.argv[3];
@@ -30,16 +35,80 @@
 //     console.log(arr[i]);
 //   }
 // });
-function callback(err, list, ext) {
-  if(err) {
-    return console.log('Error: ', err);
-  }
-  const path = require('path');
-  const arr = list.filter(element => path.extname(element) === '.' + ext);
-  for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-  }
-}
 
-const myModule = require('./myModule.js');
-myModule(process.argv[2], process.argv[3], callback);
+// 6
+// const myModule = require('./myModule.js');
+// function callback(err, list) {
+//   if(err) {
+//     return console.log('Error: ', err);
+//   }
+//   for (let i = 0; i < list.length; i++) {
+//     console.log(list[i]);
+//   }
+// }
+// myModule(process.argv[2], process.argv[3], callback);
+
+// 7
+// const http = require('http');
+//
+// const url = process.argv[2];
+//
+// http.get(url, response => {
+//   response.setEncoding('utf8')
+//   response.on('data', data => {
+//     console.log(data);
+//   });
+// });
+
+// 8a
+// const http = require('http');
+//
+// const url = process.argv[2];
+//
+// http.get(url, response => {
+//   const dataArr = [];
+//   response.setEncoding('utf8')
+//   response.on('data', data => {
+//     dataArr.push(data);
+//   });
+//   response.on('end', () => {
+//     const dataStr = dataArr.join('');
+//     console.log(dataStr.length);
+//     console.log(dataStr);
+//   });
+// });
+//
+// 8b
+// const http = require('http');
+// const bl = require('bl');
+//
+// const url = process.argv[2];
+//
+// http.get(url, response => {
+//   response.pipe(bl((error, data) => {
+//     if (error) {
+//       return console.error(error);
+//     }
+//     data = String(data);
+//     console.log(data.length);
+//     console.log(data);
+//   }));
+// });
+
+// 9
+const http = require('http');
+const bl = require('bl');
+
+const urls = [process.argv[2], process.argv[3], process.argv[4]];
+
+http.get(urls, response)
+
+
+
+
+
+
+
+
+
+//
