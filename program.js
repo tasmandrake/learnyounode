b743b803c831d1de17844153a2649cf264a8a750
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-05-27 13:46:15 -0600

    Completed learnyounode

--- program.js
+++ program.js
@@ -1,6 +1,7 @@
 // 1
 // console.log("HELLO WORLD");
 
+
 // 2
 // var sum = 0;
 // for (let i = 0; i < process.argv.length; i++) {
@@ -10,10 +11,12 @@
 // }
 // console.log(sum);
 
+
 // 3
 // const fs = require('fs');
 // const file = fs.readFileSync(process.argv[2]);
 // console.log(file.toString().split('\n').length - 1);
+
 
 // 4
 // const fs = require('fs');
@@ -25,6 +28,7 @@
 //   console.log(breaks);
 // });
 
+
 // 5
 // const fs = require('fs');
 // const path = require('path');
@@ -35,6 +39,7 @@
 //     console.log(arr[i]);
 //   }
 // });
+
 
 // 6
 // const myModule = require('./myModule.js');
@@ -48,6 +53,7 @@
 // }
 // myModule(process.argv[2], process.argv[3], callback);
 
+
 // 7
 // const http = require('http');
 //
@@ -59,6 +65,7 @@
 //     console.log(data);
 //   });
 // });
+
 
 // 8a
 // const http = require('http');
@@ -95,20 +102,123 @@
 //   }));
 // });
 
+
 // 9
-const http = require('http');
-const bl = require('bl');
-
-const urls = [process.argv[2], process.argv[3], process.argv[4]];
-
-http.get(urls, response)
-
-
-
-
-
-
-
-
-
+// const http = require('http');
+// const bl = require('bl');
 //
+// const urls = [process.argv[2], process.argv[3], process.argv[4]];
+// const dataArr = [];
+// let count = 0;
+//
+// for (let i = 0; i < urls.length; i ++) {
+//   http.get(urls[i], (response) => {
+//     response.pipe(bl((error, data) => {
+//       if (error) {
+//         return console.error(error);
+//       }
+//       dataArr[i] = (String(data));
+//       count++;
+//       if (count === 3) {
+//         for (let i = 0; i < dataArr.length; i++) {
+//           console.log(dataArr[i]);
+//         }
+//       }
+//     }));
+//   });
+// }
+
+
+// 10
+// const net = require('net');
+// const strftime = require('strftime');
+//
+// const port = process.argv[2];
+// const server = net.createServer(socket => {
+//   const strftimeMDT = strftime.timezone('-0600');
+//   socket.end(strftimeMDT('%F %R') + '\n');
+// });
+//
+// server.listen(port);
+
+
+// 11
+// const http = require('http');
+// const fs = require('fs');
+//
+// const filePath = process.argv[3];
+// const server = http.createServer((req, res) => {
+//   fs.readFile(filePath, (err, data) => {
+//     if (err) {
+//       return console.error(err);
+//     }
+//     res.end(data);
+//   });
+// });
+// server.listen(process.argv[2]);
+//
+// 11 answer
+// const http = require('http');
+// const fs = require('fs');
+//
+// const server = http.createServer(function (req, res) {
+//   res.writeHead(200, { 'content-type': 'text/plain' });
+//   fs.createReadStream(process.argv[3]).pipe(res);
+// });
+// server.listen(Number(process.argv[2]));
+
+
+// 12
+// const http = require('http');
+// const map = require('through2-map');
+//
+// const port = process.argv[2];
+//
+// const server = http.createServer((req, res) => {
+//   req.pipe(map(chunk => String(chunk).toUpperCase())).pipe(res);
+// });
+//
+// server.listen(port);
+
+
+13
+const http = require('http');
+const url = require('url');
+
+const port = process.argv[2];
+
+const server = http.createServer((req, res) => {
+  const urlObj = url.parse(req.url);
+  const query = urlObj.query;
+  const path = urlObj.pathname;
+  const method = req.method;
+  if (path === '/api/parsetime' && method === 'GET') {
+    res.end(parseTime(query));
+  } else if (path === '/api/unixtime' && method === 'GET') {
+    res.end(unixTime(query));
+  } else {
+    res.writeHead(404, {'Content-Type': 'plain/text'}).end();
+  }
+});
+
+
+function parseTime(query) {
+  const time = query.slice(15, query.length - 5).split(':');
+  console.log(time);
+  const timeObj =  {
+    'hour': Number(time[0]) - 6,
+    'minute': Number(time[1]),
+    'second': Number(time[2]),
+  };
+  return JSON.stringify(timeObj);
+}
+
+function unixTime(query) {
+  const date = {
+    unixtime: Date.parse(query.slice(4, query.length - 1)),
+  }
+  return JSON.stringify(date);
+
+}
+
+server.listen(port);
